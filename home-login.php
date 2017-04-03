28510b43aaf23786489af8aba1e2494ba9c30a16
luffysy <1337725581@qq.com>
2017-04-04 01:11:50 +0800

    初始化项目

---
+++ home-login.php
@@ -0,0 +1,271 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>兄弟连论坛主页</title>
+		<link href=".\styles\main.css" rel="stylesheet" type="text/css">
+	</head>
+	<body>
+		<!--头部-->
+		<div id="header">
+			<!--头部主导航条-->
+			<div id="nav">
+				<div id="navbox">
+					<div id="navleft">
+						<a href="3.html">设为主页</a>
+						<a href="3.html">收藏LAMP兄弟连</a>
+					</div>
+					<div id="navright">
+						<a href="3.html">搜索</a>
+						<a href="3.html">统计列表</a>
+						<a href="3.html">会员列表</a>
+						<a href="3.html">社区服务</a>
+						<a href="3.html">精华区</a>
+						<a href="3.html">最新帖子</a>
+						<a href="3.html">社区应用</a>
+						<a href="3.html">推广链接</a>
+						<a href="3.html">帮助</a>
+					</div>
+				</div>
+			</div>
+			<!--头部logo条-->
+			<div id="logo">
+				<div style="float:left">
+					<a href="home.html" target="_self"><img src="./images/logo.png" alt="兄弟连" title="兄弟连it教育http://www.itxdl.cn"></a>
+				</div>
+				<div style="float:left;margin:40px 30px 0"><img src="./images/wxdbjc.gif" ></div>
+			
+			<!--登陆信息-->
+				<div id="logined">
+					<table>
+						<tr>
+							<td width="60px"><img src="./images/phplogo.jpg" height="50px" width="50px"></td>
+							<td height="30px"> 
+								<a href="#">哈哈</a><br/>
+								<a href="home.html">注销</a>
+							</td>
+							<td height="30px"> 
+								<a href="#">幼儿园<span>(136)</span></a><br/>
+								<a href="password-change.html">个人中心</a>
+							</td>
+						</tr>
+					</table>
+				</div>
+			</div>
+			<!--头部次导航条-->
+			<div id="subnav">
+				<div id="subnavbox">
+					<div id="subnav-list">
+						<ul>
+							<li><a href="3.html">IOS培训</a></li>
+							<li><a href="3.html">HTML培训</a></li>
+							<li><a href="3.html">UI培训</a></li>
+							<li><a href="3.html">安卓培训</a></li>
+							<li><a href="3.html">兄弟连课堂</a></li>
+							<li><a href="3.html">PHP视频</a></li>
+							<li><a href="3.html">LINUX视频</a></li>
+							<li><a href="3.html">战地日记</a></li>
+						</ul>
+					</div>	
+				</div>
+			</div>
+		</div>
+		<!--主体-->
+		<div id="main">
+			<!--banner-->
+			<div id="banner">
+				<div id="training"> <a href="3.html"><img src="./images/banner-1.jpg" alt="包吃包住"> </a></div>
+				<div id="schoolhistory"><a href="3.html"> <img src="./images/banner-2.jpg" alt="学校故事"></a></div>
+				<div id="qq"><a href="3.html"> <img src="./images/banner-4.jpg" alt="联系我们"></a></div>
+				<div id="wechat"><a href="3.html"><img src="./images/banner-5.jpg" alt="加入兄弟连"></a></div>
+				<div id="freefile"><a href="3.html"><img src="./images/banner-3.png" alt="免费学习资料"></a></div>
+			</div>
+			<!--技术交流的主体-->
+			<div class="discuss" id="discuss1">
+				<!--技术交流的导航条-->
+				<div class="discussnav" id="discussnav1" >
+				<div class="discussnav-name">:::.技术交流.:::</div>
+				</div>
+				<!--技术交流六个模块开始-->
+				<div class="discusscontent" id="discusscontent1">
+				<table>
+					<!--技术交流模块第一行-->
+					<tr>
+						<td>
+						<div class="discuss-content-logo"><img class="discuss-content-image" src="./images/dislogo.jpg" ></div>
+						<div class="discuss-centent-info">
+							<a href="post.html"><p class="dicuss-content-info-theme">PHP</p></a>
+							<div>
+							<span class="dicuss-content-info-detail">帖子 24</span>
+							<span class="dicuss-content-info-detail" style="float:right;">主题 16</span>
+							</div>
+							<a href="3.html"><p class="dicuss-content-info-detail" >最后发帖：2016-05-04 15:18:04</p></a>
+						</div>
+						</td>
+						<td>
+						<div class="discuss-content-logo"><img class="discuss-content-image" src="./images/dislogo.jpg" ></div>
+						<div class="discuss-centent-info">
+							<a href="post.html"><p class="dicuss-content-info-theme">PHP</p>
+							</a><div>
+							<span class="dicuss-content-info-detail">帖子 24</span>
+							<span class="dicuss-content-info-detail" style="float:right;">主题 16</span>
+							</div>
+							<a href="3.html"><p class="dicuss-content-info-detail" >最后发帖：2016-05-04 15:18:04</p></a>
+						</div>
+						</td>
+						<td>
+						<div class="discuss-content-logo"><img class="discuss-content-image" src="./images/dislogo.jpg" ></div>
+						<div class="discuss-centent-info">
+							<a href="post.html"><p class="dicuss-content-info-theme">JAVsA</p>
+							</a><div>
+							<span class="dicuss-content-info-detail">帖子 24</span>
+							<span class="dicuss-content-info-detail" style="float:right;">主题 16</span>
+							</div>
+							<a href="3.html"><p class="dicuss-content-info-detail" >最后发帖：2016-05-04 15:18:04</p></a>
+						</div>
+						</td>
+					</tr>
+					<!--技术交流模块第二行-->
+					<tr>
+						<td>
+						<div class="discuss-content-logo"><img class="discuss-content-image" src="./images/dislogo.jpg" ></div>
+						<div class="discuss-centent-info">
+							<a href="post.html"><p class="dicuss-content-info-theme">PHP</p>
+							</a><div>
+							<span class="dicuss-content-info-detail">帖子 24</span>
+							<span class="dicuss-content-info-detail" style="float:right;">主题 16</span>
+							</div>
+							<a href="3.html"><p class="dicuss-content-info-detail" >最后发帖：2016-05-04 15:18:04</p></a>
+						</div>
+						</td>
+						<td>
+						<div class="discuss-content-logo"><img class="discuss-content-image" src="./images/dislogo.jpg" ></div>
+						<div class="discuss-centent-info">
+							<a href="post.html"><p class="dicuss-content-info-theme">PHP</p>
+							</a><div>
+							<span class="dicuss-content-info-detail">帖子 24</span>
+							<span class="dicuss-content-info-detail" style="float:right;">主题 16</span>
+							</div>
+							<a href="3.html"><p class="dicuss-content-info-detail" >最后发帖：2016-05-04 15:18:04</p></a>
+						</div>
+						</td>
+						<td>
+						<div class="discuss-content-logo"><img class="discuss-content-image" src="./images/dislogo.jpg" ></div>
+						<div class="discuss-centent-info">
+							<a href="post.html"><p class="dicuss-content-info-theme">PHP</p>
+							</a><div>
+							<span class="dicuss-content-info-detail">帖子 24</span>
+							<span class="dicuss-content-info-detail" style="float:right;">主题 16</span>
+							</div>
+							<a href="3.html"><p class="dicuss-content-info-detail" >最后发帖：2016-05-04 15:18:04</p></a>
+						</div>
+						</td>
+					</tr>	
+				</table>
+				</div>
+				<!--技术交流六个模块结束-->
+			</div>
+			<!--华山论剑-->
+				<div class="discuss" id="discuss2">
+				<!--华山论剑的导航条-->
+				<div class="discussnav" id="discussnav2" >
+					<div class="discussnav-name">:::.华山论剑.:::</div>
+				</div>
+				<div class="discusscontent" id="discusscontent1">
+				<table>
+					<!--华山论剑六个模块开始-->
+					<tr>
+						<td>
+						<div class="discuss-content-logo"><img class="discuss-content-image" src="./images/dislogo.jpg" ></div>
+						<div class="discuss-centent-info">
+							<a href="post.html"><p class="dicuss-content-info-theme">PHP</p>
+							</a><div>
+							<span class="dicuss-content-info-detail">帖子 24</span>
+							<span class="dicuss-content-info-detail" style="float:right;">主题 16</span>
+							</div>
+							<a href="3.html"><p class="dicuss-content-info-detail" >最后发帖：2016-05-04 15:18:04</p></a>
+						</div>
+						</td>
+						<td>
+						<div class="discuss-content-logo"><img class="discuss-content-image" src="./images/dislogo.jpg" ></div>
+						<div class="discuss-centent-info">
+							<a href="post.html"><p class="dicuss-content-info-theme">PHP</p>
+							</a><div>
+							<span class="dicuss-content-info-detail">帖子 24</span>
+							<span class="dicuss-content-info-detail" style="float:right;">主题 16</span>
+							</div>
+							<a href="3.html"><p class="dicuss-content-info-detail" >最后发帖：2016-05-04 15:18:04</p></a>
+						</div>
+						</td>
+						<td>
+						<div class="discuss-content-logo"><img class="discuss-content-image" src="./images/dislogo.jpg" ></div>
+						<div class="discuss-centent-info">
+							<a href="post.html"><p class="dicuss-content-info-theme">PHP</p>
+							</a><div>
+							<span class="dicuss-content-info-detail">帖子 24</span>
+							<span class="dicuss-content-info-detail" style="float:right;">主题 16</span>
+							</div>
+							<a href="3.html"><p class="dicuss-content-info-detail" >最后发帖：2016-05-04 15:18:04</p></a>
+						</div>
+						</td>
+					</tr>
+					<tr>
+						<td>
+						<div class="discuss-content-logo"><img class="discuss-content-image" src="./images/dislogo.jpg" ></div>
+						<div class="discuss-centent-info">
+							<a href="post.html"><p class="dicuss-content-info-theme">PHP</p>
+							</a><div>
+							<span class="dicuss-content-info-detail">帖子 24</span>
+							<span class="dicuss-content-info-detail" style="float:right;">主题 16</span>
+							</div>
+							<a href="3.html"><p class="dicuss-content-info-detail" >最后发帖：2016-05-04 15:18:04</p></a>
+						</div>
+						</td>
+						<td>
+						<div class="discuss-content-logo"><img class="discuss-content-image" src="./images/dislogo.jpg" ></div>
+						<div class="discuss-centent-info">
+							<a href="post.html"><p class="dicuss-content-info-theme">PHP</p>
+							</a><div>
+							<span class="dicuss-content-info-detail">帖子 24</span>
+							<span class="dicuss-content-info-detail" style="float:right;">主题 16</span>
+							</div>
+							<a href="3.html"><p class="dicuss-content-info-detail" >最后发帖：2016-05-04 15:18:04</p></a>
+						</div>
+						</td>	
+					</tr>
+					<!--华山论剑六个模块结束-->
+				</table>
+				</div>
+			</div>
+		</div>
+		<!--尾部-->
+			<!--友情链接-->
+		<div id="partner">
+				<div class="partnernav" id="parternav" >
+					<div class="discussnav-name">:::.友情链接.:::</div>
+				</div>
+				<div class="partnercontent" id="partnercontent">
+				<ul class="partnerlist">
+					<li><a href="3.html" target="_blank">PHP培训</a></li>
+					<li><a href="3.html" target="_blank">PHP培训</a></li>
+					<li><a href="3.html" target="_blank">PHP培训</a></li>
+					<li><a href="3.html" target="_blank">PHP培训</a></li>
+					<li><a href="3.html" target="_blank">PHP培训</a></li>
+					<li><a href="3.html" target="_blank">PHP培训</a></li><li><a href="3.html" target="_blank">PHP培训</a></li>
+					<li><a href="3.html" target="_blank">PHP培训</a></li>
+					<li><a href="3.html" target="_blank">PHP培训</a></li>
+					<li><a href="3.html" target="_blank">PHP培训</a></li>
+					<li><a href="3.html" target="_blank">PHP培训</a></li>
+					<li><a href="3.html" target="_blank">PHP培训</a></li>
+					<li><a href="3.html" target="_blank">PHP培训</a></li>
+					<li><a href="3.html" target="_blank">PHP培训</a></li>
+					<li><a href="3.html" target="_blank">PHP培训</a></li>
+				</ul>
+				</div>
+			</div>	
+		<div id="footer">
+			<p>Powered by scort Certificate Copyright Time now is:08-08 21:08:17</p>
+			<p>LAMPzip disabled 京ICP备11043214743号 京公网备4793201870</p>
+		</div>
+	</body>
+</html>
